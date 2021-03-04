7890f5320797b25cfed6719b3f3e15b69e9f5461
satyam <2019335@iiitdmj.ac.in.com>
2021-03-04 15:29:54 +0530

    Image Adjustment

--- core/src/com/zappycode/coinman/game/CoinMan.java
+++ core/src/com/zappycode/coinman/game/CoinMan.java
@@ -9,17 +9,24 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class CoinMan extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture background;
+	Texture man[];
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		background = new Texture("bg.png");
+		man = new Texture[4];
+		man[0] = new Texture("frame-1.png");
+		man[1] = new Texture("frame-2.png");
+		man[2] = new Texture("frame-3.png");
+		man[3] = new Texture("frame-4.png");
 	}
 
 	@Override
 	public void render () {
 		batch.begin();
 		batch.draw(background,0,0,Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
+		batch.draw(man[0],Gdx.graphics.getWidth()/2,Gdx.graphics.getHeight()/2);
 		batch.end();
 
 	}
