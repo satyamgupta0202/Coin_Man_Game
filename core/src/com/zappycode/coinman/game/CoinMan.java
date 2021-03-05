9d7e18601f2c1ed804f21bca3301f3cba44ecd9f
satyam <2019335@iiitdmj.ac.in.com>
2021-03-05 18:26:42 +0530

    To_THE_ENd

--- core/src/com/zappycode/coinman/game/CoinMan.java
+++ core/src/com/zappycode/coinman/game/CoinMan.java
@@ -2,8 +2,10 @@ package com.zappycode.coinman.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Intersector;
 import com.badlogic.gdx.math.Rectangle;
@@ -15,14 +17,15 @@ import java.util.Random;
 public class CoinMan extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture background;
+	BitmapFont font;
 	Texture man[];
 	int manstate=0;
 	int pause =0;
 	float velocity = 0;
 	float gravity = 0.2f;
-	float manY = 0;
-	int coinCount = 0;
-	int bombCount = 0 ;
+	float manY = 0 ;
+	int coinCount =0;
+	int bombCount =0 ;
 	ArrayList <Integer> coinsX = new ArrayList<>();
 	ArrayList <Integer> coinsY = new ArrayList<>();
 	ArrayList <Integer> bombsX = new ArrayList<>();
@@ -33,7 +36,8 @@ public class CoinMan extends ApplicationAdapter {
 	Texture bomb;
 	Random random;
 	Rectangle manRectangle;
-	
+	int score = 0;
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
@@ -48,6 +52,9 @@ public class CoinMan extends ApplicationAdapter {
 		manY=Gdx.graphics.getHeight() / 2;
 		manRectangle = new Rectangle();
 		random = new Random();
+		font = new BitmapFont();
+		font.setColor(Color.WHITE);
+		font.getData().scale(10);
 	}
 
 	public void makeBomb(){
@@ -68,15 +75,9 @@ public class CoinMan extends ApplicationAdapter {
 		batch.begin();
 		batch.draw(background,0,0,Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
 
-		if(coinCount<100){
-			coinCount++;
-		}
-		else{
-			coinCount=0;
-			makeCoin();
-		}
 
-		if(bombCount<240){
+		//bomb
+		if(bombCount<260){
 			bombCount++;
 		}
 		else{
@@ -84,20 +85,31 @@ public class CoinMan extends ApplicationAdapter {
 			makeBomb();
 		}
 
+		bombRectangle.clear();
+		for(int i=0;i<bombsX.size();i++){
+			batch.draw(bomb,bombsX.get(i),bombsY.get(i));
+			bombsX.set(i,bombsX.get(i)-8);
+			bombRectangle.add(new Rectangle(bombsX.get(i),bombsY.get(i),bomb.getWidth(),bomb.getHeight()));
+		}
+
+		//coins
+
+		if(coinCount < 100){
+			coinCount++;
+		}
+		else{
+			coinCount=0;
+			makeCoin();
+		}
 
 		coinRectangle.clear();
 		for(int i=0;i<coinsX.size();i++){
 			batch.draw(coin,coinsX.get(i),coinsY.get(i));
-			coinsX.set(i,coinsX.get(i)-4);
-			coinRectangle.add(new Rectangle(coinsX.get(i),coinsY.get(i),Gdx.graphics.getHeight(),Gdx.graphics.getWidth()));
+			coinsX.set(i,coinsX.get(i)-6);
+			coinRectangle.add(new Rectangle(coinsX.get(i),coinsY.get(i),coin.getWidth(),coin.getHeight()));
 		}
 
-		bombRectangle.clear();
-		for(int i=0;i<bombsX.size();i++){
-			batch.draw(bomb,bombsX.get(i),bombsY.get(i));
-			bombsX.set(i,bombsX.get(i)-7);
-			bombRectangle.add(new Rectangle(bombsX.get(i),bombsY.get(i),Gdx.graphics.getHeight(),Gdx.graphics.getWidth()));
-		}
+
 
 		if(Gdx.input.justTouched()){
 			velocity=-10;
@@ -118,12 +130,18 @@ public class CoinMan extends ApplicationAdapter {
 		if(manY<=0){
 			manY=0;
 		}
+
 			batch.draw(man[manstate], Gdx.graphics.getWidth() / 2 - man[manstate].getWidth() / 2, manY);
-			manRectangle = new Rectangle(Gdx.graphics.getWidth() / 2 - man[manstate].getWidth() / 2, manY , Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
+			manRectangle = new Rectangle(Gdx.graphics.getWidth() / 2 - man[manstate].getWidth() / 2, manY , man[manstate].getWidth(),man[manstate].getHeight());
 
 			for(int i=0;i<coinRectangle.size();i++){
 				if(Intersector.overlaps(manRectangle,coinRectangle.get(i))){
-					Gdx.app.log("coins-Overlapped","COLLISION!!!!!");
+					//Gdx.app.log("coins-Overlapped","COLLISION!!!!!");
+					score++;
+					coinRectangle.remove(i);
+					coinsX.remove(i);
+					coinsY.remove(i);
+					break;
 				}
 			}
 
@@ -132,6 +150,8 @@ public class CoinMan extends ApplicationAdapter {
 				Gdx.app.log("Bomb-Overlapped","COLLISION!!!!!");
 			}
 		}
+
+		font.draw(batch,String.valueOf(score),100,200);
 			batch.end();
 	}
 	@Override
